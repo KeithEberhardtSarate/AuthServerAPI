cca0264db2750e34d24a0996b2464c02077995e8
keith eberhardt sarate <keithsarate.info@gmail.com>
2023-08-28 12:20:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ const DATABASE_URL = process.env.DATABASE_URL
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  const now = new Date();
+  console.log(`${now.toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 
